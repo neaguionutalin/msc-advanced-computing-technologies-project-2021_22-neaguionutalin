e31b117ea0b9446e36f8433a9d915f7695f0b86d
ANeagu <alin.neagu@ardonaghspecialty.com>
2022-06-06 16:20:20 +0100

    first commit

---
+++ eldorado-mocking-service/src/main/java/com/alin/eldorado/vendormockingservice/EldoradoMockingServiceApplication.java
@@ -0,0 +1,12 @@
+package com.alin.eldorado.vendormockingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldoradoMockingServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EldoradoMockingServiceApplication.class, args);
+  }
+}
---
+++ eldorado-mocking-service/src/test/java/com/alin/eldorado/vendormockingservice/EldoradoMockingServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.alin.eldorado.vendormockingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldoradoMockingServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
