99c9e70c498de03ba7bb2e3c1fbc9497a500bd77
ANeagu <alin.neagu@ardonaghspecialty.com>
2022-06-13 11:30:43 +0100

    template service to be used for other services

---
+++ eldorado-service_template/component_tests/src/test/java/com/eldorado/BaseComponentTest.java
@@ -0,0 +1,21 @@
+package com.eldorado;
+
+import com.eldorado.activemq.EmbeddedActiveMQWrapper;
+import com.eldorado.changetoservicename.Application;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.camel.test.spring.CamelSpringBootRunner;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+
+@RunWith(CamelSpringBootRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = Application.class)
+@Slf4j
+public abstract class BaseComponentTest {
+
+  @BeforeClass
+  public static void createBroker() throws Exception {
+    EmbeddedActiveMQWrapper.startBroker();
+  }
+}
