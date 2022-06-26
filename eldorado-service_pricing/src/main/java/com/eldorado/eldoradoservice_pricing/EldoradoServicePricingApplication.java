447bfdc9f3c9d9821d8c296a7d75b1ed917cb531
ANeagu <alin.neagu@ardonaghspecialty.com>
2022-06-26 17:50:49 +0100

    pricing service init

---
+++ eldorado-service_pricing/src/main/java/com/eldorado/eldoradoservice_pricing/EldoradoServicePricingApplication.java
@@ -0,0 +1,12 @@
+package com.eldorado.eldoradoservice_pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldoradoServicePricingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EldoradoServicePricingApplication.class, args);
+  }
+}
---
+++ eldorado-service_pricing/src/test/java/com/eldorado/eldoradoservice_pricing/EldoradoServicePricingApplicationTests.java
@@ -0,0 +1,11 @@
+package com.eldorado.eldoradoservice_pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldoradoServicePricingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
