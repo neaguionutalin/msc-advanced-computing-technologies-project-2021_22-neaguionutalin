1bcc221689c40312004b8149a407bdfd5df646ce
ANeagu <alin.neagu@ardonaghspecialty.com>
2022-07-17 15:42:01 +0100

    client gateway added. pending implementation

---
+++ eldorado-service_client_gateway/src/main/java/com/example/eldoradoservice_client_gateway/EldoradoServiceClientGatewayApplication.java
@@ -0,0 +1,12 @@
+package com.example.eldoradoservice_client_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldoradoServiceClientGatewayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EldoradoServiceClientGatewayApplication.class, args);
+  }
+}
---
+++ eldorado-service_client_gateway/src/test/java/com/example/eldoradoservice_client_gateway/EldoradoServiceClientGatewayApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.eldoradoservice_client_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldoradoServiceClientGatewayApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
